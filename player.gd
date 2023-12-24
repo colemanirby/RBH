b9d15a9e102a6368c776a5f9846dce16e1ed46e6
Coleman Irby <ckirbyee92@gmail.com>
2023-12-24 14:40:46 -0600

    ship moves much smoother now

--- player.gd
+++ player.gd
@@ -4,7 +4,9 @@ extends Area2D
 @export var acceleration = 1600
 @export var deceleration = 100
 @export var shot_delay = 0.1
+var velocity_increment = .075
 var previous_velocity
+var velocity_direction_vector = Vector2.ZERO
 var Bullet = preload("res://bullet.tscn")
 var screen_size
 var can_fire = true
@@ -49,25 +51,69 @@ func _physics_process(_delta):
 		current_input.y += 1
 	if Input.is_action_pressed("move_up"):
 		current_input.y -= 1
+	
 
-	var velocity = calc_veloc(current_input, _delta)
+	var velocity = calc_veloc_2(current_input, _delta)
 	#velocity = velocity.normalized() * max_speed
 	#$AnimatedSprite2D.play(
 	position += velocity * _delta
 	previous_velocity = velocity
 	position = position.clamp(Vector2.ZERO, screen_size)
 	
+func calc_veloc_2(current_input: Vector2, delta):
+	var v_x = velocity_direction_vector.x
+	var v_y = velocity_direction_vector.y
+	var v_len = velocity_direction_vector.length()
+	if current_input.length() >= 1:
+		# player has input something
+		if(v_len >= 1):
+			handle_full_velocity(current_input, v_x, v_y)
+		else:
+			accumulate_velocity(current_input)
+	else:
+		reduce_velocity()
+	return velocity_direction_vector * max_speed
+	
+func handle_full_velocity(current_input, v_x, v_y):
+	var x = current_input.x
+	var y = current_input.y
+	velocity_direction_vector += Vector2(x * velocity_increment, y * velocity_increment)
+	velocity_direction_vector = velocity_direction_vector.normalized()
+
+func reduce_velocity():
+	velocity_direction_vector = velocity_direction_vector - Vector2(0.1*velocity_direction_vector.x, 0.1*velocity_direction_vector.y)
+	
+func accumulate_velocity(current_input):
+	var x = current_input.x
+	var y = current_input.y
+	velocity_direction_vector += Vector2(x * velocity_increment, y * velocity_increment)
+	
+func handle_decrement_velocity(current_input):
+	
+	if velocity_direction_vector.x > -1:
+		print("decrement")
+		velocity_direction_vector -= Vector2(velocity_increment, 0)
+	pass
+	
+func handle_increment_velocity(current_input):
+	if velocity_direction_vector.x < 1:
+		print("increment")
+		velocity_direction_vector += Vector2(velocity_increment, 0)
+	pass
 	
 # Speed up if requested. Slow down otherwise.
 func calc_veloc(current_input, delta):
 	var final_velocity_vector = previous_velocity
 	var final_speed = 0
 	if current_input.length() > 0:
-		if previous_velocity.length() < max_speed:
-			final_speed = previous_velocity.length() + (acceleration * delta)
-			final_velocity_vector = current_input.normalized() * final_speed
-		else:
-			final_velocity_vector = current_input.normalized() * max_speed
+		velocity_direction_vector.x += current_input.x * velocity_increment
+		#if previous_velocity.length() < max_speed:
+			#final_speed = previous_velocity.length() + (acceleration * delta)
+			#final_velocity_vector = final_velocity_vector + current_input.normalized() * final_speed
+		#else:
+			#final_velocity_vector = current_input.normalized() * max_speed
+		final_speed = previous_velocity.length() + (velocity_direction_vector.x * acceleration * delta)
+		final_velocity_vector = velocity_direction_vector.normalized() * final_speed
 	else:
 		final_speed = previous_velocity.length() - (deceleration * delta)
 		final_velocity_vector = final_velocity_vector.normalized() * final_speed
