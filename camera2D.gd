54824811ccd377c119f8da61e769eae67ff67788
colemanirby <ckirbyee92@gmail.com>
2023-12-28 23:48:30 -0600

    cleaned up if conditions for zoom

--- camera2D.gd
+++ camera2D.gd
@@ -6,15 +6,17 @@ var zoom_min = 1
 var current_zoom
 
 func _ready():
+	#just to be safe
 	current_zoom = zoom.x + zoom.y
 	
 func _input(event):
-	current_zoom = zoom.x + zoom.y
-	print("current_zoom: ",current_zoom)
-	if event.is_action_pressed("zoom_in") and current_zoom < zoom_max:
-		zoom += zoom_increment
-		print("zoom in: ", zoom)
-	elif event.is_action_pressed("zoom_out") and current_zoom > zoom_min:
-		zoom -= zoom_increment
-		print("zoom out: ", zoom)
-	print("zoom: ", zoom)
+	if event.is_action_pressed("zoom_in"):
+		current_zoom = zoom.x + zoom.y
+		if current_zoom < zoom_max:
+			zoom += zoom_increment
+			print("zoom in: ", zoom)
+	elif event.is_action_pressed("zoom_out"):
+		current_zoom = zoom.x + zoom.y
+		if current_zoom > zoom_min:
+			zoom -= zoom_increment
+			print("zoom out: ", zoom)
